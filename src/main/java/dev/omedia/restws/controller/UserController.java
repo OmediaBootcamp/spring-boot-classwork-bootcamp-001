cc0f8be173e08682dbf970acfd0c2d9d35923fe2
Nikoloz Chapidze <nick.chapidze@gmail.ge>
2022-08-11 14:00:20 +0400

    spring dockerization springdoc swagger ui
    Annotations
    @Operation
    @ApiResponses
    @Parameter
    
    javax.validation.constraints

--- src/main/java/dev/omedia/restws/controller/UserController.java
+++ src/main/java/dev/omedia/restws/controller/UserController.java
@@ -4,12 +4,21 @@ import dev.omedia.restws.config.Config;
 import dev.omedia.restws.exceptions.UserNotFoundException;
 import dev.omedia.restws.model.User;
 import dev.omedia.restws.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -26,11 +35,14 @@ public class UserController {
         this.config = config;
     }
 
+    @Operation(summary = "Get users")
+    @ApiResponses(value =
+            {@ApiResponse(responseCode = "200", description = "Found users", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500",description = "Server error", content = {@Content(mediaType = "application/json")})})
     @GetMapping
-    public Collection<User> getUsers(
-            @RequestParam(required = false, defaultValue = "${page}", name = "p") int page
-            , @RequestParam(required = false, defaultValue = "${pageSize}", name = "s") int pageSize) {
-        log.error("page{} pageSize{}",page,pageSize);
+    public Collection<User> getUsers(@Min(value = 1,message = "incorrect value") @Parameter(description = "page index")@RequestParam(required = false, defaultValue = "${page}", name = "p") int page,
+                                     @Min(30)@Max(500)@Parameter(description = "page size")@RequestParam(required = false, defaultValue = "${pageSize}", name = "s") int pageSize) {
+        log.error("page{} pageSize{}", page, pageSize);
         log.error(config.toString());
         log.trace("trace");
         log.debug("debug");
