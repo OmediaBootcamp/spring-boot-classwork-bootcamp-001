ef70bfafea0cca14c3ab968360884fec2f70f204
giorgiodishvili <godishvili@newage.io>
2022-09-23 11:57:57 +0400

    added localdatetime audit

--- src/main/java/dev/omedia/domain/Audit.java
+++ src/main/java/dev/omedia/domain/Audit.java
@@ -3,12 +3,14 @@ package dev.omedia.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "audit")
@@ -19,5 +21,9 @@ public class Audit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @CreatedDate
+    private LocalDateTime localDateTime;
+
     private String updates;
 }
